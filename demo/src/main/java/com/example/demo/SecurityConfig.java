593bc501f6624891b9369c82f266dbf8d104e161
ChirayuP19 <chirayu010675@gmail.com>
2025-12-30 09:31:30 -0500

    add 2 users and admin

--- demo/src/main/java/com/example/demo/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -5,15 +5,37 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests)->requests.anyRequest().authenticated());
+   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request->request.anyRequest().authenticated());
+    http.sessionManagement(session->session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
-    }
+}
+@Bean
+public UserDetailsService userDetailsService(){
+    UserDetails user1= User.withUsername("user1")
+            .password("{noop}password1")
+            .roles("USER")
+            .build();
+
+    UserDetails admin=User.withUsername("admin")
+            .password("{noop}adminPass")
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user1,admin);
+}
 }
